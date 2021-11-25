9c0a4835cf13bdf47e683eaf36642b887f7c9f29
kalyanbhargav1432 <tannirukalyan1432@gmail.com>
2021-11-25 12:32:08 -0800

    Added:Functionality to print read numbers

---
+++ ReadNumbers.js
@@ -0,0 +1,19 @@
+let digit = 10
+let one =1
+let ten =10
+let hundred =100
+let thousand = 1000
+switch(digit){
+    case one:
+        console.log("one");
+        break;
+    case ten:
+        console.log("Ten");
+        break;
+    case hundred:
+        console.log("Hundred");
+        break;
+    case thousand:
+        console.log("Thousand");
+        break;
+}
